48af4b634eedb4d5f1949c9621361ee4b81dc54a
reson320 <107577827+reson320@users.noreply.github.com>
2022-06-26 19:53:57 +0800

    kattis

---
+++ filip.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b;
+    cin >> a >>b;
+    reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    if(a>b) cout << a;
+    else cout << b;
+	
+}
