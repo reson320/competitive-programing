1637eaa91c8a029e462b1bd28f0bf0080fc15572
reson320 <107577827+reson320@users.noreply.github.com>
2022-06-26 20:15:07 +0800

    kattis

---
+++ dicecup.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(a>b){
+        swap(a,b);
+    }
+    for(int i=a+1;i<b+2;i++){
+        cout << i << endl;
+    }
+	
+}
